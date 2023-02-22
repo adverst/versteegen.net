5c3546d1e85d04588af45080ce8524a242c51898
Adam verst <web@invarion.com>
2023-02-22 11:45:59 -0500

    template

---
+++ src/versteegen.net.client/Pages/Godot.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace versteegen.net.client.Pages
+{
+    public partial class Godot
+    {
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+        public async void StartGodot()
+        {
+            var cancelTok = new CancellationToken();
+            await JSRuntime.InvokeVoidAsync("launchGodot", cancelTok,"canvas");
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (!firstRender)
+            {
+                return;
+            }
+            var cancelTok = new CancellationToken();
+            await JSRuntime.InvokeVoidAsync("launchGodot", cancelTok, "canvas");
+        }
+    }
+}
